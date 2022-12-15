1a842eb6c2b314c01f7e1a727c7f1753b0fa26c8
Dmitrii <razuhindm@gmail.com>
2022-12-15 22:26:12 +0300

    ломашняя работа

---
+++ phone_book.py
@@ -0,0 +1,21 @@
+phone_book = []
+
+
+def get_phone_book():
+    global phone_book
+    return phone_book
+
+def set_phone_book(new_phone_book:list):
+    global phone_book
+    phone_book = new_phone_book
+
+def add_contact(contact):
+    global phone_book
+    phone_book.append(contact)
+
+def remove_contact(id):
+    global phone_book
+    name=phone_book[id-1][0]
+    confirm = input(f'Вы действительно хотите удалить контакт {name}? y/n')
+    if confirm.lower=='y':
+        phone_book.pop()
---
+++ view.py
@@ -0,0 +1,52 @@
+def main_menu():
+    print('\n1. Показать телефонную книгу')
+    print('2. Открыть файл телефонной книги:')
+    print('3. Сохранить файл телефонной книги')
+    print('4. Добавить контакт телефонной книги')
+    print('5. Изменить контакт')
+    print('6. Удалить контактк:')
+    print('7. Найти контакт')
+    print('0. Выход\n')
+    return choice_main_menu()
+
+def choice_main_menu():
+    while True:
+        try:
+            choice=int(input('Выберите пункт меню: '))
+            if choice in range(0,8):
+                print()
+                return choice
+            else:
+                print('Такого пункта меню нет, повтроите ввод')
+
+        except:
+            print('Ошибка ввода, некорректные данные')
+
+def print_phone_book(phone_book:list):
+    if len(phone_book) >0:
+        for id,contact in enumerate(phone_book):
+            print(id,contact)
+    else:
+        print('Телефонная книга пуста, или не загружена')
+
+def log_off():
+    print('До свидания')
+
+def load_success():
+    print('Телефонна книга загружена')
+
+def save_success():
+    print('Телефонна книга сохранена')
+
+def remove_success():
+    print('Контакт успешно удален')
+
+def input_new_contact():
+    name = input('Введите имя контакта ')
+    phone = input('Введите телефон контакта ')
+    comment = input('Введите комментарий к контакту ')
+    return [name,phone,comment]
+
+def input_remove_contact():
+    id = int(input('Ввeди ID контакта'))
+    return id
